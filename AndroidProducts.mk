6346bf77a768de76405d15a63819958027c97762
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-07-30 11:45:40 +0200

    ASUS_AI2205: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: asus Model: ASUS_AI2205 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_ASUS_AI2205.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_ASUS_AI2205-user \
+    omni_ASUS_AI2205-userdebug \
+    omni_ASUS_AI2205-eng
---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for asus ASUS_AI2205 (ASUS_AI2205)
+
+```
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
