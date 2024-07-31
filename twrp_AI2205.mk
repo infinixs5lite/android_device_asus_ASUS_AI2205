7554fe6f9e687a2882ec840f436ace52e752f991
nazephyrus <126683488+naden01@users.noreply.github.com>
2024-07-31 03:31:43 -0700

    Update and rename twrp_ASUS_AI2205.mk to twrp_AI2205.mk

--- twrp_AI2205.mk
+++ twrp_AI2205.mk
@@ -7,16 +7,16 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from ASUS_AI2205 device
 $(call inherit-product, device/asus/ASUS_AI2205/device.mk)
 
-PRODUCT_DEVICE := ASUS_AI2205
-PRODUCT_NAME := omni_ASUS_AI2205
+PRODUCT_DEVICE := AI2205
+PRODUCT_NAME := twrp_AI2205
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_AI2205
 PRODUCT_MANUFACTURER := asus
