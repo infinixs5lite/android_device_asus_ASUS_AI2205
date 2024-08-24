46f92c6632a636460db42e84ef67eb70be59aac4
Debarpan102 <91316829+Debarpan102@users.noreply.github.com>
2024-08-24 13:23:57 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -84,6 +84,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
